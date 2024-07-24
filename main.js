abe6b9f0930ecc03b1ee9e66166b403804b8ec71
eemenalop <eemena26@gmail.com>
2024-07-24 11:49:23 -0400

    printReport finished

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-const {crawlPage} = require('./crawl.js')
+const { crawlPage } = require('./crawl.js')
+const { printReport } = require('./report.js')
 
-async function main (){
-    if (process.argv.length < 3){
+
+async function main() {
+    if (process.argv.length < 3) {
         console.log("no website provided")
         process.exit(1)
     }
-    if (process.argv.length > 3){
+    if (process.argv.length > 3) {
         console.log("too many command lin args")
         process.exit(1)
     }
-    
+
     const baseURL = process.argv[2]
     console.log(`starting crawl of ${baseURL}`)
     const pages = await crawlPage(baseURL, baseURL, {})
 
-    for (const page of Object.entries(pages)) {
-        console.log(page)
-    }
+    printReport(pages)
 }
 
 
